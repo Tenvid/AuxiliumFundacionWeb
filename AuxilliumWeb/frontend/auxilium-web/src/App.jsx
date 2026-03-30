58549b68c49505678a2f02fc871f64768f4a331c
Carlos Chacón <chaconmooncarlos@gmail.com>
2026-03-30 12:41:27 +0200

    feat(project-page): create project page
    need to merge the changes on main
    
    BREAKING CHANGE:

--- AuxilliumWeb/frontend/auxilium-web/src/App.jsx
+++ AuxilliumWeb/frontend/auxilium-web/src/App.jsx
@@ -1,82 +1,17 @@
-import './App.css'
-import main_image from "./assets/jessica-neves-sbMIZxxhgbw-unsplash.jpg"
-import { MainNav } from './components/MainNav'
-import { TextImage } from './components/TextImage'
-import logo_dark from "./assets/LogoAuxiliumVector--dark.svg"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import MainPage from './pages/main-page/MainPage.jsx'
+import ProjectsPage from './pages/projects-page/ProjectsPage.jsx'
 
 function App() {
 
-  const loadGridImages = () => {
-    const gridImages = [];
-    const images = [
-      main_image,
-      main_image,
-      main_image,
-      main_image,
-      main_image,
-      main_image,
-    ]
-
-    const gridImagesText = [
-      'Lorem Ipsum dolor sit amet',
-      'Lorem Ipsum dolor sit amet',
-      'Lorem Ipsum dolor sit amet',
-      'Lorem Ipsum dolor sit amet',
-      'Lorem Ipsum dolor sit amet',
-      'Lorem Ipsum dolor sit amet',
-    ]
-
-    for (let i = 0; i < 6; i++) {
-      gridImages.push(
-        <TextImage
-          image={images[i]}
-          key={i}>
-          {gridImagesText[i]}
-        </TextImage>
-      );
-    }
-
-    return gridImages
-  }
-  const gridImages = loadGridImages()
-
-  return (
-    <>
-      <MainNav></MainNav>
-
-      <TextImage image={main_image}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec fringilla leo.
-      </TextImage>
-
-      <section className='image-grid-wrapper'>
-        <div className='image-grid'>
-          {gridImages}
-        </div>
-      </section>
-
-      <section>
-        <div className='footer-image__header-wrapper'>
-          <h4 className='footer-image__header'>
-            COLABORA
-          </h4>
-
-          <div className='footer-image__button-wrapper'>
-            <a href="#" className='footer-image__button'>
-              COLABORA CON NOSOTROS
-            </a>
-          </div>
-        </div>
-
-        <TextImage image={main_image}>
-          Forma parte del cambio:
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac pretium  mi, eget vulputate ante. Integer sodales, mauris at laoreet posuere,  erat libero tempus diam, quis varius massa massa a.</TextImage>
-      </section>
-
-      <footer className='footer-wrapper'>
-        <img src={logo_dark} alt="" />
-      </footer>
-    </>
-  )
+    return (
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainPage/>}/>
+          <Route path='/projects' element={<ProjectsPage/>}/>
+        </Routes>
+      </Router>
+    )
 }
 
 export default App
---
+++ AuxilliumWeb/frontend/auxilium-web/src/pages/projects-page/ProjectsPage.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from 'react'
+import { MainNav } from '../../components/MainNav'
+import logo_dark from "../../assets/LogoAuxiliumVector--dark.svg"
+import styles from './ProjectsPage.module.css'
+import projectsData from '../../../mocked_results/mocked_projects.json'
+
+const ProjectStyleComponents = {
+    styleOne: ({ text1, text2, image1, image2 }) => (
+        <div className='Project1'>
+            <div className={styles.ProjectType1}>
+                <div className={styles.ProjectOneWrapperOne}>
+                    <p>{text1}</p>
+                    <img src={image1} alt=""/>
+                </div>
+                <div className={styles.ProjectOneWrapperTwo}>
+                    <img src={image2} alt=""/>
+                    <p>{text2}</p>
+                </div>
+            </div>
+        </div>
+    ),
+    styleTwo: ({ text1, text2, image1 }) => (
+        <div className='Project2'>
+            <div className={styles.ProjectType2}>
+                <div className={styles.ProjectTwoWrapper}>
+                    <p>{text1}</p>
+                    <img src={image1} alt=""/>
+                </div>
+                <p className={styles.ProjectTwoButtomText}>{text2}</p>
+            </div>
+        </div>
+    ),
+    styleThree: ({ text1, image1, image2 }) => (
+        <div className='Project3'>
+            <div className={styles.ProjectThreeWrapper}>
+                <p className={styles.ProjectThreeText}>{text1}</p>
+                <div className={styles.ProjectThreePhotoWrapper}>
+                    <img src={image1} alt=""/>
+                    <img src={image2} alt=""/>
+                </div>
+            </div>
+        </div>
+    ),
+    styleFour: ({ text1, text2, image1 }) => (
+        <div className='Project4'>
+            <div className={styles.ProjectFourWrapper}>
+                <p>{text1}</p>
+                <img src={image1} className={styles.ProjectFourImage} alt=""/>
+                <p>{text2}</p>
+            </div>
+        </div>
+    )
+}
+
+function GenerateProject(title = "", text1 = "", text2 = "", image1 = "", image2 = "", stylefour = false) {
+    const tileElement = <div className={styles.ProjectHeader}>{title}</div>
+    
+    let styleKey = null
+    
+    if (stylefour) {
+        styleKey = 'styleFour'
+    } else if (text1 && text2 && image1 && image2) {
+        styleKey = 'styleOne'
+    } else if (text1 && text2 && image1) {
+        styleKey = 'styleTwo'
+    } else if (text1 && image1 && image2) {
+        styleKey = 'styleThree'
+    }
+    
+    const Component = styleKey ? ProjectStyleComponents[styleKey] : null
+    
+    return Component ? (
+        <>
+            {tileElement}
+            <Component text1={text1} text2={text2} image1={image1} image2={image2} />
+        </>
+    ) : null
+}
+
+function ProjectsPage() {
+    const [projects, setProjects] = useState(projectsData)
+    
+    useEffect(() => {
+        console.log(projects) // Verifica que el JSON se cargó correctamente
+    }, [])
+
+    return (
+        <div>
+            <MainNav/>
+            {GenerateProject(projects.Project1.Title, projects.Project1.Text1, projects.Project1.Text2, projects.Project1.Image1, projects.Project1.Image2)}
+            {GenerateProject(projects.Project2.Title,projects.Project2.Text1,projects.Project2.Text2,projects.Project2.Image1)}
+            {GenerateProject(projects.Project3.Title,projects.Project3.Text1,"",projects.Project3.Image1,projects.Project3.Image2)}
+            {GenerateProject(projects.Project4.Title,projects.Project4.Text1,projects.Project4.Text2,projects.Project4.Image1,"",true)}
+            <footer className={styles.footerWrapper}>
+                <img src={logo_dark} alt="" />
+            </footer>
+        </div>
+
+    )
+}
+export default ProjectsPage
