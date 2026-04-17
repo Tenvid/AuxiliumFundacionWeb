666297fbe032dd9aa7692b9dded41423d01745ce
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-04-17 18:23:55 +0200

    feat: add link to transparency page

--- AuxilliumWeb/frontend/auxilium-web/src/App.jsx
+++ AuxilliumWeb/frontend/auxilium-web/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/main-page/MainPage.jsx';
 import ProjectsPage from './pages/projects-page/ProjectsPage.jsx';
 import ContactPage from './pages/contact-page/ContactPage.jsx';
+import TransparencyPage from './pages/transparency-page/TransparencyPage.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/transparency" element={<TransparencyPage />}></Route>
       </Routes>
     </Router>
   );
