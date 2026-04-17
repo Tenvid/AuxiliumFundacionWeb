f9228f8acf7e6f1ad52005cb6f0177871e190994
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-04-17 17:32:57 +0200

    feat: add images to main page

--- AuxilliumWeb/frontend/auxilium-web/src/components/TextImage/TextImage.jsx
+++ AuxilliumWeb/frontend/auxilium-web/src/components/TextImage/TextImage.jsx
@@ -1,17 +1,16 @@
-import styles from "./TextImage.module.css"
-
+import styles from './TextImage.module.css';
 
 export function TextImage({ children, image, imageAlt = null }) {
   return (
     <section className={styles.textImageWrapper}>
-      <p className={styles.textImageText}>
-        {children}
-      </p>
+      <p className={styles.textImageText}>{children}</p>
       <img
         src={image}
-        alt={imageAlt ? imageAlt : ""}
+        alt={imageAlt ? imageAlt : ''}
         className={styles.textImageContent}
+        // width={800}
+        // height={300}
       />
     </section>
-  )
+  );
 }
--- AuxilliumWeb/frontend/auxilium-web/src/pages/main-page/MainPage.jsx
+++ AuxilliumWeb/frontend/auxilium-web/src/pages/main-page/MainPage.jsx
@@ -1,22 +1,20 @@
-import './MainPage.css'
-import main_image from "../../assets/jessica-neves-sbMIZxxhgbw-unsplash.jpg"
-import {MainNav} from '../../components/MainNav/MainNav'
-import {TextImage}  from '../../components/TextImage/TextImage'
-import {ImageWithHeaderAndButton} from '../../components/ImageWithHeaderAndButton/ImageWithHeaderAndButton.jsx'
-import logo_dark from "../../assets/LogoAuxiliumVector--dark.svg"
+import './MainPage.css';
+import main_image from '../../assets/jessica-neves-sbMIZxxhgbw-unsplash.jpg';
+import { MainNav } from '../../components/MainNav/MainNav';
+import { TextImage } from '../../components/TextImage/TextImage';
+import { ImageWithHeaderAndButton } from '../../components/ImageWithHeaderAndButton/ImageWithHeaderAndButton.jsx';
+import logo_dark from '../../assets/LogoAuxiliumVector--dark.svg';
+import image_1 from '../../assets/gallery/projects/CampanaDonacionComida/CampanaDonacionComida_1.jpeg';
+import image_2 from '../../assets/gallery/projects/CampanaDonacionRopa/CampanaDonacionRopa_1.jpeg';
+import image_3 from '../../assets/gallery/projects/CampanaDonacionRopa/CampanaDonacionRopa_2.jpeg';
+import image_4 from '../../assets/gallery/projects/PozoCamerun/PozoCamerun1.jpeg';
+import image_5 from '../../assets/gallery/projects/PozoCamerun/PozoCamerun2.jpeg';
+import image_6 from '../../assets/gallery/projects/index/ManosUnidas.jpeg';
 
 function MainPage() {
-
   const loadGridImages = () => {
     const gridImages = [];
-    const images = [
-      main_image,
-      main_image,
-      main_image,
-      main_image,
-      main_image,
-      main_image,
-    ]
+    const images = [image_1, image_2, image_3, image_4, image_5, image_6];
 
     const gridImagesText = [
       'Lorem Ipsum dolor sit amet',
@@ -25,34 +23,31 @@ function MainPage() {
       'Lorem Ipsum dolor sit amet',
       'Lorem Ipsum dolor sit amet',
       'Lorem Ipsum dolor sit amet',
-    ]
+    ];
 
     for (let i = 0; i < 6; i++) {
       gridImages.push(
-        <TextImage
-          image={images[i]}
-          key={i}>
+        <TextImage image={images[i]} key={i}>
           {gridImagesText[i]}
         </TextImage>
       );
     }
 
-    return gridImages
-  }
-  const gridImages = loadGridImages()
+    return gridImages;
+  };
+  const gridImages = loadGridImages();
 
   return (
     <>
       <MainNav></MainNav>
 
       <TextImage image={main_image}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec fringilla leo.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        fringilla leo.
       </TextImage>
 
-      <section className='image-grid-wrapper'>
-        <div className='image-grid'>
-          {gridImages}
-        </div>
+      <section className="image-grid-wrapper">
+        <div className="image-grid">{gridImages}</div>
       </section>
 
       <section>
@@ -65,11 +60,11 @@ function MainPage() {
         />
       </section>
 
-      <footer className='footer-wrapper'>
-        <img src={logo_dark} className='footer-logo' alt="" />
+      <footer className="footer-wrapper">
+        <img src={logo_dark} className="footer-logo" alt="" />
       </footer>
     </>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
