59d29ecee331d77ae79d1b1cdd797a0503e920ba
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-05-01 20:01:29 +0200

    feat(about us): add intro text and values

---
+++ AuxilliumWeb/frontend/auxilium-web/src/components/AboutUsCard/AboutUsCard.jsx
@@ -0,0 +1,9 @@
+import styles from './AboutUsCard.module.css';
+export default function AboutUsCard({ title, children }) {
+  return (
+    <div className={styles.aboutUsCard}>
+      <h3 className={styles.aboutUsCard__Title}>{title}</h3>
+      <p>{children}</p>
+    </div>
+  );
+}
--- AuxilliumWeb/frontend/auxilium-web/src/pages/about-us/AboutUs.jsx
+++ AuxilliumWeb/frontend/auxilium-web/src/pages/about-us/AboutUs.jsx
@@ -1,11 +1,48 @@
 import styles from './AboutUs.module.css';
+import AboutUsCard from '../../components/AboutUsCard/AboutUsCard';
+import UnorderedList from '../../components/UnorderedList/UnorderedList';
 import { MainNav } from '../../components/MainNav/MainNav';
-
+import main_image from '../../assets/jessica-neves-sbMIZxxhgbw-unsplash.jpg';
 export default function AboutUs() {
   return (
     <>
       <MainNav />
-      <h1 className={styles.aboutUsTitle}>¿Quiénes somos?</h1>
+      <div className={styles.aboutUsContainer}>
+        <h1 className={styles.aboutUsTitle}>¿Quiénes somos?</h1>
+        <img
+          src={main_image}
+          alt="Imagen de fondo"
+          className={styles.aboutUsMainImage}
+        />
+        <p className={styles.aboutUsParagraph}>
+          Nuestra misión es promover el desarrollo humano sostenible mediante
+          proyectos de cooperación internacional que mejoren las condiciones de
+          vida de comunidades en situación de vulnerabilidad, especialmente en
+          el acceso al agua potable, la educación, la salud y el desarrollo
+          comunitario.
+        </p>
+        <p className={styles.aboutUsParagraph}>
+          Aspiramos a un mundo más justo en el que todas las personas tengan
+          acceso a recursos básicos, oportunidades de desarrollo y una vida
+          digna.
+        </p>
+        <h2 className={styles.aboutUsSubtitle}>Nuestros Valores</h2>
+        <div className={styles.aboutUsCardsContainer}>
+          <AboutUsCard title="Transparencia">
+            Gestionamos los recursos de forma responsable y clara.
+          </AboutUsCard>
+          <AboutUsCard title="Solidaridad">
+            Trabajamos con el compromiso de apoyar a quienes más lo necesitan
+          </AboutUsCard>
+          <AboutUsCard title="Cooperación">
+            Colaboramos con organizaciones locales y comunidades
+          </AboutUsCard>
+          <AboutUsCard title="Respeto cultural">
+            Respetamos las tradiciones y la identidad de cada comunidad
+          </AboutUsCard>
+        </div>
+        <h2 className={styles.aboutUsSubtitle}>Nuestros Objetivos</h2>
+      </div>
     </>
   );
 }
