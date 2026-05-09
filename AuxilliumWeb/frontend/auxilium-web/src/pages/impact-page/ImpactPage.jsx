9b55691bf894532cab5ae86b826fc1d3dd115a3e
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-05-09 14:39:40 +0200

    feat: add introduction

--- AuxilliumWeb/frontend/auxilium-web/src/pages/impact-page/ImpactPage.jsx
+++ AuxilliumWeb/frontend/auxilium-web/src/pages/impact-page/ImpactPage.jsx
@@ -2,12 +2,26 @@ import Footer from '../../components/Footer/Footer/Footer';
 import { MainNav } from '../../components/MainNav/MainNav';
 import { TextImage } from '../../components/TextImage/TextImage';
 import main_image from '../../assets/jessica-neves-sbMIZxxhgbw-unsplash.jpg';
+import styles from './ImpactPage.module.css';
 
 export default function ImpactPage() {
   return (
     <>
       <MainNav />
       <TextImage image={main_image}>Impacto de la asociación</TextImage>
+      <h3>Nuestro impacto en 2026</h3>
+      <h4>
+        En 2026 mas de 2000 personas mejorarón su situación personal gracias a
+        los proyectos de nuestra asociación
+      </h4>
+
+      <img src={main_image} alt="" className={styles.headerImage} />
+      <div className={styles.paragraphsWrapper}>
+        <span>+1000 Personas ayudadas</span>
+        <span>+500 Nuevas oportunidades creadas</span>
+        <span>+10000€ Destinados a proyectos de integración social</span>
+      </div>
+
       <Footer />
     </>
   );
