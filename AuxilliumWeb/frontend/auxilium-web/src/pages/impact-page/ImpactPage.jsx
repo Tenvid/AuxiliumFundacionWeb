c8390c5afb1d2b3fefa3388aca25bcdfb73541c8
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-05-09 14:13:22 +0200

    feat: add impact page link

---
+++ AuxilliumWeb/frontend/auxilium-web/src/pages/impact-page/ImpactPage.jsx
@@ -0,0 +1,14 @@
+import Footer from '../../components/Footer/Footer/Footer';
+import { MainNav } from '../../components/MainNav/MainNav';
+import { TextImage } from '../../components/TextImage/TextImage';
+import main_image from '../../assets/jessica-neves-sbMIZxxhgbw-unsplash.jpg';
+
+export default function ImpactPage() {
+  return (
+    <>
+      <MainNav />
+      <TextImage image={main_image}>Texto de la imagen</TextImage>
+      <Footer />
+    </>
+  );
+}
