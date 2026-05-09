d3a08dd7955a805e1329b717bb30d85a38bdf148
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-05-09 11:20:32 +0200

    style(about us): center values section in mobile

--- AuxilliumWeb/frontend/auxilium-web/src/components/AboutUsCard/AboutUsCard.jsx
+++ AuxilliumWeb/frontend/auxilium-web/src/components/AboutUsCard/AboutUsCard.jsx
@@ -1,9 +1,9 @@
 import styles from './AboutUsCard.module.css';
 export default function AboutUsCard({ title, children }) {
   return (
-    <div className={styles.aboutUsCard}>
+    <li className={styles.aboutUsCard}>
       <h3 className={styles.aboutUsCard__Title}>{title}</h3>
       <p>{children}</p>
-    </div>
+    </li>
   );
 }
