2e2ceec8528eaa6043336ecf3418e32dc5ccfd43
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-05-24 18:16:17 +0200

    feat: add endpoint for get all projects

--- AuxilliumWeb/core/urls.py
+++ AuxilliumWeb/core/urls.py
@@ -3,4 +3,5 @@ from core import views as core_views
 
 urlpatterns = [
     path("", core_views.IndexView.as_view(), name="index"),
+    path("projects/", core_views.get_projects, name="projects"),
 ]
--- AuxilliumWeb/core/views.py
+++ AuxilliumWeb/core/views.py
@@ -1,5 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+from core.models import Project
 # Create your views here.
 
 
@@ -11,3 +13,17 @@ class HomeView(View):
 class IndexView(View):
     def get(self, request):
         return render(request, "core/index.html")
+
+
+def get_projects(request):
+    projects = Project.objects.all()
+    data = []
+    for project in projects:
+        data.append(
+            {
+                "title": project.title,
+                "paragraph_list": project.paragraph_list,
+                "image_list": project.image_list,
+            }
+        )
+    return JsonResponse(data, safe=False)
