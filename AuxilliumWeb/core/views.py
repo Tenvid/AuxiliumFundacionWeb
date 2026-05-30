8fe9a26b0dc7606db3b33158be6fc11c88088719
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-05-30 15:45:17 +0200

    feat: add view for project creation

--- AuxilliumWeb/core/views.py
+++ AuxilliumWeb/core/views.py
@@ -1,6 +1,8 @@
+import json
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 from core.models import Project
 # Create your views here.
 
@@ -16,14 +18,41 @@ class IndexView(View):
 
 
 def get_all_projects(request):
-    projects = Project.objects.all()
-    data = []
-    for project in projects:
-        data.append(
+    if request.method == "GET":
+        projects = Project.objects.all()
+        data = []
+        for project in projects:
+            data.append(
+                {
+                    "title": project.title,
+                    "paragraph_list": project.paragraph_list,
+                    "image_list": project.image_list,
+                }
+            )
+        return JsonResponse(data, safe=False)
+
+
+@csrf_exempt
+def add_project(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+
+        title = data.get("title")
+        paragraph_list = data.get("paragraph_list")
+        image_list = data.get("image_list")
+
+        project = Project.objects.create(
+            title=title,
+            paragraph_list=paragraph_list,
+            image_list=image_list,
+        )
+        return JsonResponse(
             {
                 "title": project.title,
                 "paragraph_list": project.paragraph_list,
                 "image_list": project.image_list,
             }
         )
-    return JsonResponse(data, safe=False)
