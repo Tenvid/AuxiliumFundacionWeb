0fc4716c7a4c736ad17d567d4395baccc4bbb9c5
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-05-30 15:45:23 +0200

    feat: add url for project creation

--- AuxilliumWeb/core/urls.py
+++ AuxilliumWeb/core/urls.py
@@ -4,4 +4,5 @@ from core import views as core_views
 urlpatterns = [
     path("", core_views.IndexView.as_view(), name="index"),
     path("projects/", core_views.get_all_projects, name="projects"),
+    path("projects/add", core_views.add_project, name="add_project"),
 ]
